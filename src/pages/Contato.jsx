addb2d56ee3b9321af25358740a09e97359e7650
Kevinn <kevinnvannucci0@gmail.com>
2024-11-17 23:52:03 -0300

    Atualizado

--- src/pages/Contato.jsx
+++ src/pages/Contato.jsx
@@ -1,13 +1,13 @@
 import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
 
-
 function Contato(){
     return (
-        <>
+        <div>
             <Header/>
+                
             <Footer/>
-        </>
+        </div>
     )
 }
 export default Contato;
