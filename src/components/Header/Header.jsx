81a1c00ade278cc7b8fd5ce81c93ca2f3ace6848
Fernando <fernandorgoncalves@hotmail.com>
2024-12-15 21:13:48 -0300

    Ajustando e melhorando o Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,40 +1,44 @@
-import {Link} from 'react-router-dom'
-import "./Header.css"
+import { Link } from 'react-router-dom';
+import './Header.css';
 
-//assets
-import logo from "../../assets/logo.svg"
+// Assets
+import logo from '../../assets/logo.svg';
 
-function Header(){
-    return 
-      main
-        <section className="primary-background">
+function Header() {
+    return (
+        <header className="primary-background">
             <div className="container">
-                <nav>
-                    <ul className=' nav-menu d-flex fd-row jc-space-between al-center '>
-                        <div className='d-flex fd-row al-center t-partner'>
-                        <img src={logo}></img>
-                        <p className='tertiary-color'>Partinership</p>
-                        </div>
-                        <li><Link to={"/"} className='primary-color nav-link'>Home</Link></li>
-                        <li><Link to={"/comofunciona"} className='primary-color nav-link'>Como Funciona</Link></li>
-                        <li><Link to={"/contato"} className='primary-color nav-link'>Contato</Link></li>
-                        <li><Link to="/login" className='primary-color nav-link'>Entre ou Cadastre-se</Link></li>
-        <div className=" container-nav primary-background">
-            <div className="container d-flex jc-space-between">
-                <div className="nav-logo d-flex">
-                    <img src={logo}></img>
-                </div>
-                <div className="nav-menu">
-                    <ul>
-                        <li><Link to={"/"} className='primary-color'>Home</Link></li>
-                        <li><Link to={"/comofunciona"} className='primary-color'>Como Funciona</Link></li>
-                        <li><Link to={"/contato"} className='primary-color'>Contato</Link></li>
-                        <li><Link to={"/login"} className='primary-color'>Entre ou Cadastre-se</Link></li>
-     main
+                <nav className="d-flex">
+                    <div className=" nav-menu d-flex fd-row al-center  t-partner">
+                        <img src={logo} alt="Logo" className="logo" />
+                        <p className="tertiary-color">Partnership</p>
+                    </div>
+                    <ul className="nav-menu d-flex fd-row jc-space-between al-center">
+                        <li>
+                            <Link to="/" className="primary-color nav-link">
+                                Home
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/comofunciona" className="primary-color nav-link">
+                                Como Funciona
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/contato" className="primary-color nav-link">
+                                Contato
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/login" className="primary-color nav-link">
+                                Entre ou Cadastre-se
+                            </Link>
+                        </li>
                     </ul>
-                </nav>      
-                </div>
-        </section>
-    )
+                </nav>
+            </div>
+        </header>
+    );
 }
+
 export default Header;
